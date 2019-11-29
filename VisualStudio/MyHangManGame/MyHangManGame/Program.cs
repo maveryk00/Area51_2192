23acd66705fe7c655e8e3ef9e254b4059c29febf
Felipe Jimenez <maveryk00@gmail.com>
2019-11-28 22:04:59 -0500

    Primer juego terminado

--- VisualStudio/MyHangManGame/MyHangManGame/Program.cs
+++ VisualStudio/MyHangManGame/MyHangManGame/Program.cs
@@ -9,6 +9,55 @@ namespace MyHangManGame
     class Program
     {
         static void Main(string[] args) {
+
+            string secretWord = "zapatos";
+            string publicWord = "";
+            string tempWord = "";
+            string letter = "";
+            int hp = 3;
+            
+
+            for (int i = 0; i < secretWord.Length; i++) {
+                tempWord += "*";
+            }
+            publicWord = tempWord;
+            Console.WriteLine(publicWord);
+
+
+            while (hp > 0) {
+                Console.WriteLine("HP = " + hp);
+                Console.WriteLine("Ingrese una letra:");
+                letter = Console.ReadLine();
+                Console.Clear();
+
+                tempWord = "";
+                for (int i = 0; i < secretWord.Length; i++) {
+                    if (letter == "" + secretWord[i]) {
+                        tempWord += letter; //tempWord += secretWord[i];
+                    }
+                    else {
+                        tempWord += publicWord[i];
+                    }
+                }
+
+                if (publicWord == tempWord) {
+                    hp--;
+                }
+
+                if(secretWord == tempWord) {
+                    Console.WriteLine("YOU WIN!!");
+                    break;
+                }
+
+                publicWord = tempWord;
+                Console.WriteLine(publicWord);
+            }
+
+            if (hp <= 0) {
+                Console.WriteLine("GAME OVER");
+            }
+            
+            Console.ReadLine();
         }
     }
 }
