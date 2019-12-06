18b35d896ac8eaadccd825a50add37866fdaf7b8
Felipe Jimenez <maveryk00@gmail.com>
2019-12-05 21:33:17 -0500

    Code base

---
+++ VisualStudio/ClassExercises/ClassExercises/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercises
+{
+    class Stats
+    {
+        public int STR;
+        public int DEX;
+        public int CON;
+    }
+}
