c128dc09d88738f722f609e5730fbecffb70658e
Felipe Jimenez <maveryk00@gmail.com>
2019-12-17 19:22:29 -0500

    jump

--- Unity2d/Platformer 2d/Assets/Scripts/Player.cs
+++ Unity2d/Platformer 2d/Assets/Scripts/Player.cs
@@ -2,43 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     [SerializeField]
     private SpriteRenderer spriteRenderer;
-    private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rbody2D;
 
     public float speed = 1f;
     public Animator animator;
 
+    public bool grounded { get { return RoundAbsoluteToZero(rbody2D.velocity.y) == 0f; } }
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float h = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-
-        //if (h != 0)
-        //    animator.SetBool("walk", true);
-        //else
-        //    animator.SetBool("walk", false);
-
         animator.SetBool("walk", (h != 0));
+        animator.SetBool("jump", !grounded);
+        animator.SetFloat("vertical", Mathf.Sign(rbody2D.velocity.y));
 
-        if (h > 0)
-            spriteRenderer.flipX = false;
-        else if (h < 0)
-            spriteRenderer.flipX = true;
+        if (h != 0) { spriteRenderer.flipX = (h < 0); }
 
-        transform.Translate(Vector3.right * h * speed);
+        //transform.Translate(Vector3.right * h * speed);
+        MyTranslate(Vector3.right * h * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            rigidbody2D.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+            rbody2D.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
     }
 
+    void MyTranslate(Vector3 translateVector) {
+        transform.localPosition += translateVector;
+    }
+
+    float RoundAbsoluteToZero(float decimalValue) {
+        decimalValue = Mathf.Abs(decimalValue);
+        if (decimalValue <= 0.01f) {
+            decimalValue = 0f;
+        }
+        return decimalValue;
+    }
 }
