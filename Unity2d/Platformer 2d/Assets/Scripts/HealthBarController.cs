ce0941aa54df71b50fac52731f8db45f918add0d
Felipe Jimenez <maveryk00@gmail.com>
2020-01-09 20:48:15 -0500

    HealthBar v2

--- Unity2d/Platformer 2d/Assets/Scripts/HealthBarController.cs
+++ Unity2d/Platformer 2d/Assets/Scripts/HealthBarController.cs
@@ -5,37 +5,62 @@ using UnityEngine.UI;
 
 public class HealthBarController : MonoBehaviour
 {
-    public Image barStart;
-    public Image barMiddle;
-    public Image barEnd;
+    private float minScale = 0f;
+    private float maxScale = 30f;
+
+    public RectTransform barMiddle;
+    public RectTransform barEnd;
 
     public float maxLife = 50;
-    public float currentLife;
+    public float _currentLife;
+
+    public float currentLife {
+        set {
+            _currentLife = Mathf.Clamp(value, 0, maxLife);
+            UpdateBar();
+        }
+
+        get {
+            return _currentLife;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        currentLife = maxLife;
+        _currentLife = maxLife;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.O)) {
-            //bar.fillAmount -= 0.1f;
             currentLife--;
         }
 
         if (Input.GetKeyDown(KeyCode.P)) {
-            //bar.fillAmount += 0.1f;
             currentLife++;
         }
 
-        currentLife = Mathf.Clamp(currentLife, 0, maxLife);
-        bar.fillAmount = currentLife / maxLife;
     }
 
     void UpdateBar() {
+        float percentLife = _currentLife / maxLife;
+        float percentScale = maxScale * percentLife;
 
+        Vector3 scale = barMiddle.localScale;
+        scale.x = Mathf.Clamp(percentScale, minScale, maxScale);
+        Vector3 pos = barEnd.localPosition;
+        pos.x = barMiddle.localPosition.x + 
+                    (barMiddle.sizeDelta.x * scale.x);
+        barEnd.localPosition = pos;
+        
+        barMiddle.localScale = scale;
     }
+
+    //public void SetLife(float life) {
+    //    _currentLife = life;
+    //    UpdateBar();
+    //}
+
 }
