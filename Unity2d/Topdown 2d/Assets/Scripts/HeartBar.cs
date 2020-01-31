d1c2fa3f8ad6e9f9ad7b421d238aa8bb7d9b27bc
Felipe Jimenez <maveryk00@gmail.com>
2020-01-30 19:42:21 -0500

    Heartbar add namespace

--- Unity2d/Topdown 2d/Assets/Scripts/HeartBar.cs
+++ Unity2d/Topdown 2d/Assets/Scripts/HeartBar.cs
@@ -3,71 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HeartBar : MonoBehaviour {
-    public Image fullHeart;
-    public Image emptyHeart;
-    public Image[] quarterHeart;
+namespace Topdown.UI {
+    public class HeartBar : MonoBehaviour {
+        public Image fullHeart;
+        public Image emptyHeart;
+        public Image[] quarterHeart;
 
-    public int maxLife = 12;
-    public int maxHearts {
-        get {
-            return maxLife / 4;
-        }
-    }
-
-    public int currentLife = 12;
-    public int fullHearts {
-        get {
-            return currentLife / 4;
-        }
-    }
-
-    public int quarterHearts {
-        get {
-            return currentLife % 4;
-        }
-    }
-    
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentLife = 4;
-        UpdateHearts();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Clear() {
-        foreach(Transform child in transform) {
-            Destroy(child.gameObject);
-        }
-    }
-
-    public void UpdateHearts() {
-        Clear();
-
-        for (int i = 0; i < fullHearts; i++) {
-            Instantiate<Image>(fullHeart, transform);
+        public int maxLife = 12;
+        public int maxHearts {
+            get {
+                return maxLife / 4;
+            }
         }
 
-        if (quarterHearts > 0) {
-            Instantiate<Image>
-                (quarterHeart[quarterHearts], transform);
+        public int currentLife = 12;
+        public int fullHearts {
+            get {
+                return currentLife / 4;
+            }
         }
 
-        int currentHearts = fullHearts +
-            (quarterHearts > 0 ? 1 : 0);
-        
-        for (int i = 0; 
-            i < maxHearts - currentHearts;
-            i++) {
-            Instantiate<Image>(emptyHeart, transform);
+        public int quarterHearts {
+            get {
+                return currentLife % 4;
+            }
+        }
+
+
+
+        // Start is called before the first frame update
+        void Start() {
+            currentLife = 4;
+            UpdateHearts();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void Clear() {
+            foreach (Transform child in transform) {
+                Destroy(child.gameObject);
+            }
+        }
+
+        public void UpdateHearts() {
+            Clear();
+
+            for (int i = 0; i < fullHearts; i++) {
+                Instantiate<Image>(fullHeart, transform);
+            }
+
+            if (quarterHearts > 0) {
+                Instantiate<Image>
+                    (quarterHeart[quarterHearts], transform);
+            }
+
+            int currentHearts = fullHearts +
+                (quarterHearts > 0 ? 1 : 0);
+
+            for (int i = 0;
+                i < maxHearts - currentHearts;
+                i++) {
+                Instantiate<Image>(emptyHeart, transform);
+            }
         }
     }
 }
