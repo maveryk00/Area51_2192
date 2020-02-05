62039f937df5a5079140018e98cb526be0f7632a
Felipe Jimenez <maveryk00@gmail.com>
2020-02-04 20:31:58 -0500

    Move with path part I

--- Unity2d/Topdown 2d/Assets/Scripts/Patrol.cs
+++ Unity2d/Topdown 2d/Assets/Scripts/Patrol.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 namespace Topdown {
     public class Patrol : MonoBehaviour {
+        float t = 0f;
+        Vector3 startPos;
 
         public Vector2[] points;
 
         // Start is called before the first frame update
         void Start() {
-
+            startPos = transform.position;
         }
 
         // Update is called once per frame
         void Update() {
+            Vector3 point = points[0];
+            transform.position = Vector3.Lerp(startPos, startPos + point, t);
 
+            t += Time.deltaTime;
+
+            if (t >= 1f)
+                t = 0;
         }
 
         private void OnDrawGizmos() {
+            if (!Application.isPlaying && startPos != transform.position)
+                startPos = transform.position;
+
             Vector3 point = points[0];
             Debug.DrawLine(
-                transform.position,
-                transform.position + point,
+                startPos,
+                startPos + point,
                 Color.cyan);
 
             for (int i = 1; i < points.Length; i++) {
                 point = points[i-1];
-                Vector3 start = transform.position + point;
+                Vector3 start = startPos + point;
 
                 point = points[i];
-                Vector3 end = transform.position + point;
+                Vector3 end = startPos + point;
                 Debug.DrawLine(
                     start,
                     end,
@@ -38,8 +49,8 @@ namespace Topdown {
 
             point = points[points.Length-1];
             Debug.DrawLine(
-                transform.position + point,
-                transform.position,
+                startPos + point,
+                startPos,
                 Color.cyan);
         }
 
