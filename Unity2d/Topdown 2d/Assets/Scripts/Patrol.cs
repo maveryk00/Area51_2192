7f1d83cc6caae45f6c520a7b7f072c602f400d3d
Felipe Jimenez <maveryk00@gmail.com>
2020-02-06 20:30:21 -0500

    Patrol with lists

--- Unity2d/Topdown 2d/Assets/Scripts/Patrol.cs
+++ Unity2d/Topdown 2d/Assets/Scripts/Patrol.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Topdown.AI;
 
 namespace Topdown {
     public class Patrol : MonoBehaviour {
@@ -8,22 +9,21 @@ namespace Topdown {
         float t = 0f;
         Vector3 startPos;
         Vector3 endPos;
-        Vector3 point;
-        Vector3 origin;
+        List<Vector3> points;
 
-        public Vector2[] points;
         public float speed = 1f;
 
         public ActionArea player;
 
+        public Path path;
+
         // Start is called before the first frame update
         void Start() {
             currentIndex = 0;
-            point = points[currentIndex];
-            origin = transform.position;
-            startPos = origin;
-            endPos = origin + point;
-            
+            points = path.points;
+
+            startPos = points[0];
+            endPos = points[1];
         }
 
         // Update is called once per frame
@@ -32,65 +32,23 @@ namespace Topdown {
 
             t += Time.deltaTime * speed;
 
-            if (Vector3.Distance(transform.position, player.transform.position) <= player.radius) {
-                speed *= 2f;
-            }
-            else {
-                float distance = Vector3.Distance(startPos, endPos);
-                speed = 2f / distance;
-            }
-
-
+            
             if (t >= 1f) {
                 startPos = endPos;
                 currentIndex++;
 
-                if (currentIndex >= points.Length) {
-                    currentIndex = -1;
-                    point = Vector3.zero;
-                }
-                else {
-                    point = points[currentIndex];
+                if (path.isClose && currentIndex >= points.Count) {
+                    currentIndex = 0;
                 }
 
-                endPos = origin + point;
+                endPos = points[currentIndex];
 
                 float distance = Vector3.Distance(startPos, endPos);
                 speed = 2f /distance;
 
                 t = 0;
             }
-
+            
         }
-
-        private void OnDrawGizmos() {
-            if (!Application.isPlaying && origin != transform.position)
-                origin = transform.position;
-
-            Vector3 point = points[0];
-            Debug.DrawLine(
-                origin,
-                origin + point,
-                Color.cyan);
-
-            for (int i = 1; i < points.Length; i++) {
-                point = points[i - 1];
-                Vector3 start = origin + point;
-
-                point = points[i];
-                Vector3 end = origin + point;
-                Debug.DrawLine(
-                    start,
-                    end,
-                    Color.cyan);
-            }
-
-            point = points[points.Length - 1];
-            Debug.DrawLine(
-                origin + point,
-                origin,
-                Color.cyan);
-        }
-
     }
 }
