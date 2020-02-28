c6d4bb1a3e7c8fdb2ca3ecaae3e93a4009978f1f
Felipe Jimenez <maveryk00@gmail.com>
2020-02-27 22:06:01 -0500

    Player first person v1

---
+++ Unity3d/FirstPersonTowerDefense/Assets/Scripts/FPTD/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPTD {
+    public class CameraController : MonoBehaviour {
+
+        public bool invert = false;
+        public float smooth = 10f;
+
+        // Start is called before the first frame update
+        void Start() {
+            
+            
+        }
+
+        void LateUpdate() {
+            float angle = Input.GetAxis("Mouse Y");
+            angle *= smooth * Time.deltaTime;
+
+            if (invert)
+                angle *= -1;
+
+            transform.Rotate(
+                Vector3.right, 
+                angle
+                );
+
+            
+        }
+    }
+}
---
+++ Unity3d/FirstPersonTowerDefense/Assets/Scripts/FPTD/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPTD {
+    public class Player : MonoBehaviour {
+        public float speed = 1f;
+        public float torque = 100f;
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            Vector3 pos = new Vector3(
+                Input.GetAxisRaw("Horizontal"),
+                0,
+                Input.GetAxisRaw("Vertical")
+                );
+            pos *= speed * Time.deltaTime;
+
+            transform.position += pos;
+
+
+            float angle = Input.GetAxis("Mouse X");
+            angle *= torque * Time.deltaTime;
+
+            transform.Rotate(
+                Vector3.up,
+                angle
+                );
+        }
+    }
+}
